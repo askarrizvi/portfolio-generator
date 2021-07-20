82323f67877b9322144028134c730dcced05a0a2
Askar Rizvi <askarrizvi88@gmail.com>
2021-07-20 00:55:50 -0400

    Modify HTML tfor more data and generate entire website

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const inquirer = require('inquirer');
-/*
-const fs = require('fs');
 const generatePage = require('./src/page-template');
-
+const { writeFile, copyFile } = require('./utils/generate-site.js');
+/*
 const pageHTML = generatePage(name, github);
 
 fs.writeFile('./index.html', pageHTML, err => {
@@ -141,5 +140,18 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
     });
